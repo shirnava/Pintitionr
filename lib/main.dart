f6c2647d0bf46dc8dc0c2f02cec516d0ee7e9efe
shirnava <shirnava@gmail.com>
2021-11-13 16:33:59 -0500

    Added Marker Functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import 'package:geoflutterfire/geoflutterfire.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:rxdart/rxdart.dart';
+// import 'dart:async';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+// import 'package:location/location.dart';
 
- main() async {
+main() async {
   runApp(const MyApp());
 }
 
@@ -25,34 +31,90 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MapScreen(),
+      home: Scaffold(body: MapScreen()),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MapScreen extends StatefulWidget {
-  const MapScreen({ Key? key }) : super(key: key);
+  const MapScreen({Key? key}) : super(key: key);
 
   @override
   _MapScreenState createState() => _MapScreenState();
 }
 
 class _MapScreenState extends State<MapScreen> {
+  Set<Marker> _markers = {};
+  late GoogleMapController mapController;
   static const initialCam = CameraPosition(
-
-    target: LatLng(28.6024,-81.2001),
+    target: LatLng(28.6024, -81.2001),
     zoom: 15.5,
   );
+
+  CameraPosition _position = initialCam;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: GoogleMap(
+    return Stack(children: [
+      GoogleMap(
         mapType: MapType.hybrid,
         myLocationButtonEnabled: false,
         zoomControlsEnabled: false,
         initialCameraPosition: initialCam,
+        onMapCreated: _onMapCreated,
+        markers: _markers,
+        myLocationEnabled: true,
+        compassEnabled: true,
+        onCameraMove: _updateCameraPosition,
+        onTap: _handleTap,
       ),
-    );
+      // Positioned(
+      //   bottom: 50,
+      //   right: 10,
+      //   // child: FlatButton(
+      //   //     child: Icon(Icons.pin_drop, color: Colors.white),
+      //   //     color: Colors.green,
+      //   //     onPressed: _addMarker
+      //   //     ),
+      // )
+    ]);
+  }
+
+  _handleTap(LatLng tappedPoint) {
+    setState(() {
+      // _markers = {};
+      _markers.add(Marker(
+          markerId: MarkerId(tappedPoint.toString()),
+          position: tappedPoint,
+          draggable: true,
+          onDragEnd: (dragEndPosition) {
+            print(dragEndPosition);
+          },
+          infoWindow: InfoWindow(
+            title: '📋 Petitioner',
+            snippet: 'University',
+          )));
+    });
+  }
+
+  void _updateCameraPosition(CameraPosition position) {
+    setState(() {
+      _position = position;
+    });
+  }
+
+  _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      // _markers.add(
+      //   Marker(
+      //       markerId: MarkerId('id-1'),
+      //       position: LatLng(28.6024, -81.2001),
+      //       infoWindow: InfoWindow(
+      //         title: '📋 Petitioner',
+      //         snippet: 'University',
+      //       )),
+      // );
+    });
   }
 }
